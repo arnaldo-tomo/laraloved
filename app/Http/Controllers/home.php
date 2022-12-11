331a30c5ac7407984695a605a09c947dd56b8445
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2022-12-11 17:03:06 +0200

    Update home.php

--- app/Http/Controllers/home.php
+++ app/Http/Controllers/home.php
@@ -2,14 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\cidade;
 use App\Models\home as ModelsHome;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class home extends Controller
 {
     public function home()
     {
-        $homes = ModelsHome::all();
-        return view('welcome', compact('homes'));
+        $homes = ModelsHome::find(1);
+        $user = User::find(1);
+
+
+        echo $homes->d->nome;
+
+        foreach ($user->home as $dado) {
+            echo $dado->namespace . " ## " . $user;
+        }
+        // return view('welcome', compact('homes', 'user'));
     }
 }
