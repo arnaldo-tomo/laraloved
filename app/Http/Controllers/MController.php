74600d655a7f0482fcd3c2d2ed31cf8ffe7fdf78
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2022-12-12 01:11:30 +0200

    Update MController.php

--- app/Http/Controllers/MController.php
+++ app/Http/Controllers/MController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\alunos;
+use App\Models\disciplinas;
 use Illuminate\Http\Request;
 
 class MController extends Controller
 {
     public function home()
     {
+        $dado = disciplinas::find(1);
+        dd($dado->aluno->aluno);
     }
 }
