d423ac5e3e007dc4c5884e280cba48db358e6f13
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2022-12-12 01:02:32 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,11 @@
 <?php
 
 use App\Http\Controllers\home;
+use App\Http\Controllers\MController;
 use App\Models\alunos;
 use App\Models\disciplina;
 use App\Models\disciplinas;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    $dado = alunos::find(1);
-    dd($dado->disciplinas->disciplina);
-});
+
+Route::get('/', [MController::class, 'home']);
