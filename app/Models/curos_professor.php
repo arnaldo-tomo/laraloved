7ce2f600f4d3712be3f73d47875394cf29cc1487
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2022-12-12 16:51:48 +0200

    Create curos_professor.php

---
+++ app/Models/curos_professor.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class curos_professor extends Model
+{
+    use HasFactory;
+}
