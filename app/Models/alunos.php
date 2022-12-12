a933a8aa5b471532a3d32a3cd2a3fefdd66da944
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2022-12-12 11:11:19 +0200

    Update alunos.php

--- app/Models/alunos.php
+++ app/Models/alunos.php
@@ -1,18 +1,17 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\alunos;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class alunos extends Model
 {
-    protected $table = 'alunos';
-    protected $fillable = array('aluno');
+
     use HasFactory;
 
     public function disciplinas()
     {
-        return $this->belongsToMany(disciplina::class, 'disciplina_id');
+        return $this->belongsToMany(disciplina::class, 'aluno_disciplina', 'disciplina_id', 'aluno_id')->withTimestamps();
     }
 }
