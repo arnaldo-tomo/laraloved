6438e4e87d3032fe7e6277c534a8a6a72970aa9d
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2022-12-12 11:11:30 +0200

    Update disciplinas.php

--- app/Models/disciplinas.php
+++ app/Models/disciplinas.php
@@ -1,18 +1,17 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\disciplina;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class disciplinas extends Model
 {
-    protected $table = 'disciplinas';
-    protected $fillable =  array('disciplina');
+
     use HasFactory;
 
     public function aluno()
     {
-        return $this->belongsToMany(aluno::class, 'aluno_id');
+        return $this->belongsToMany(aluno::class, 'aluno_disciplina', 'aluno_id', 'disciplina_id')->withTimestamps();
     }
 }
