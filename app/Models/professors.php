718ec92009811d81442f5c494ade5d59c15dc521
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2022-12-12 17:29:54 +0200

    Update professors.php

--- app/Models/professors.php
+++ app/Models/professors.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use App\Models\alunos\alunos;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class professors extends Model
 {
     use HasFactory;
+
+    public function curso()
+    {
+        return $this->belongsToMany(alunos::class, 'cursos_professors');
+    }
 }
