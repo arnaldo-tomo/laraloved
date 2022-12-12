8c98c121fb1faace39d4123abd8c8b10f2f7c150
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2022-12-12 17:29:48 +0200

    Update MController.php

--- app/Http/Controllers/MController.php
+++ app/Http/Controllers/MController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\cursos;
 use App\Models\disciplina\disciplinas;
 
 class MController extends Controller
 {
     public function home()
     {
+
+        $dado = cursos::find(1);
+        dd($dado->professor->nome);
     }
 }
